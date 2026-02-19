bac148cda90afdb045760da36cefa950fb9824d3
Jonathan Netley <jonathan.netley@golfnow.com>
2026-02-19 10:25:51 +0200

    chore: add solution

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,18 @@ export function calculate(value: number): number | null {
   }
   return info;
 }
+
+export function calculateWithSuggested(value: number): number | null {
+  let info!: number | null; // Using definite assignment assertion to indicate that 'info' will be assigned before use: https://github.com/eslint/eslint/issues/20517#issuecomment-3925242960
+  try {
+    if (value > 10) {
+      info = value * 2;
+    } else {
+      throw new Error("Value must be greater than 10");
+    }
+  } catch (error) {
+    console.error("Error occurred calculating value", { cause: error });
+    info = null;
+  }
+  return info;
+}
